98d0eab2b77aaf96f74aef049518e92a70851cab
Deva33369 <kumar.devadharshini@gmail.com>
2025-06-05 19:52:32 +0800

    udpate

--- main.py
+++ main.py
@@ -9,7 +9,13 @@ from handlers.care_handler import (
     care, care_story, care_support_groups, care_tips, care_journaling,
     care_journaling_prompts, care_grounding, care_letters, care_younger_self
 )
-from handlers.learn_handler import learn
+from handlers.learn_handler import (
+    learn, learn_tips, learn_tips2, learn_tips3, learn_tips4,
+    volunteer, learn_sa, learn_sexual_assault, learn_sexual_grooming,
+    learn_sexual_grooming2, learn_sexual_grooming3, learn_consent,
+    learn_consent2, learn_consent3, learn_consent4, learn_victim_blaming,
+    learn_victim_blaming2, learn_rape_myths
+)
 from handlers.feedback_handler import feedback, handle_feedback
 
 async def myid(update: Update, context):
@@ -88,6 +94,25 @@ def main():
     application.add_handler(CallbackQueryHandler(care_grounding, pattern='^care_grounding$'))
     application.add_handler(CallbackQueryHandler(care_letters, pattern='^care_letters$'))
     application.add_handler(CallbackQueryHandler(care_younger_self, pattern='^care_younger_self$'))
+
+    #learn and volunteer handlers
+    application.add_handler(CallbackQueryHandler(learn_tips, pattern="^learn_tips$"))
+    application.add_handler(CallbackQueryHandler(learn_tips2, pattern="^learn_tips2$"))
+    application.add_handler(CallbackQueryHandler(learn_tips3, pattern="^learn_tips3$"))
+    application.add_handler(CallbackQueryHandler(learn_tips4, pattern="^learn_tips4$"))
+    application.add_handler(CallbackQueryHandler(volunteer, pattern="^volunteer$"))
+    application.add_handler(CallbackQueryHandler(learn_sa, pattern="^learn_sa$"))
+    application.add_handler(CallbackQueryHandler(learn_sexual_assault, pattern="^learn_sexual_assault$"))
+    application.add_handler(CallbackQueryHandler(learn_sexual_grooming, pattern="^learn_sexual_grooming$"))
+    application.add_handler(CallbackQueryHandler(learn_sexual_grooming2, pattern="^learn_sexual_grooming2$"))
+    application.add_handler(CallbackQueryHandler(learn_sexual_grooming3, pattern="^learn_sexual_grooming3$"))
+    application.add_handler(CallbackQueryHandler(learn_consent, pattern="^learn_consent$"))
+    application.add_handler(CallbackQueryHandler(learn_consent2, pattern="^learn_consent2$"))
+    application.add_handler(CallbackQueryHandler(learn_consent3, pattern="^learn_consent3$"))
+    application.add_handler(CallbackQueryHandler(learn_consent4, pattern="^learn_consent4$"))
+    application.add_handler(CallbackQueryHandler(learn_victim_blaming, pattern="^learn_victim_blaming$"))
+    application.add_handler(CallbackQueryHandler(learn_victim_blaming2, pattern="^learn_victim_blaming2$"))
+    application.add_handler(CallbackQueryHandler(learn_rape_myths, pattern="^learn_rape_myths$"))
     
     # Message handler for both story and feedback submissions
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
