8bf34e8aa9d9ab92f16fde1bb186bcb745190704
toughtootinbaby <nurshifa19@gmail.com>
2025-06-05 14:43:10 +0800

    learn and volunteer, also changed start handler so intro text does not appear all the time

--- handlers/utils.py
+++ handlers/utils.py
@@ -2,3 +2,6 @@ from telegram import InlineKeyboardButton
 
 def back_button(target='start'):
     return [InlineKeyboardButton("🔙 Back", callback_data=target)]
+
+def next_button(target='start'):
+    return [InlineKeyboardButton("➡️ Next",callback_data=target)]
--- main.py
+++ main.py
@@ -9,7 +9,14 @@ from handlers.care_handler import (
     care, care_story, care_support_groups, care_tips, care_journaling,
     care_journaling_prompts, care_grounding, care_letters, care_younger_self
 )
-from handlers.learn_handler import learn
+from handlers.learn_and_volunteer_handler import (
+    learn_and_volunteer, learn_tips, learn_tips2, learn_tips3, learn_tips4,
+    volunteer, learn_sa, learn_sexual_assault, learn_sexual_grooming, learn_sexual_grooming2, learn_sexual_grooming3,
+    learn_consent, learn_consent2, learn_consent3, learn_consent4, learn_victim_blaming, learn_victim_blaming2,
+    learn_rape_myths
+)
+
+from handlers.learn_and_volunteer_handler import learn_and_volunteer
 from handlers.feedback_handler import feedback
 
 async def handle_story(update: Update, context):
@@ -28,7 +35,7 @@ def main():
     application.add_handler(CallbackQueryHandler(help, pattern='^help$'))
     application.add_handler(CallbackQueryHandler(support, pattern='^support$'))
     application.add_handler(CallbackQueryHandler(care, pattern='^care$'))
-    application.add_handler(CallbackQueryHandler(learn, pattern='^learn$'))
+    application.add_handler(CallbackQueryHandler(learn_and_volunteer, pattern='^learn_and_volunteer$'))
     application.add_handler(CallbackQueryHandler(feedback, pattern='^feedback$'))
     
     # Care menu handlers
@@ -41,6 +48,25 @@ def main():
     application.add_handler(CallbackQueryHandler(care_letters, pattern='^care_letters$'))
     application.add_handler(CallbackQueryHandler(care_younger_self, pattern='^care_younger_self$'))
     
+    #learn and volunteer handlers
+    application.add_handler(CallbackQueryHandler(learn_tips, pattern="^learn_tips$"))
+    application.add_handler(CallbackQueryHandler(learn_tips2, pattern="^learn_tips2$"))
+    application.add_handler(CallbackQueryHandler(learn_tips3, pattern="^learn_tips3$"))
+    application.add_handler(CallbackQueryHandler(learn_tips4, pattern="^learn_tips4$"))
+    application.add_handler(CallbackQueryHandler(volunteer, pattern="^volunteer$"))
+    application.add_handler(CallbackQueryHandler(learn_sa, pattern="^learn_sa$"))
+    application.add_handler(CallbackQueryHandler(learn_sexual_assault, pattern="^learn_sexual_assault$"))
+    application.add_handler(CallbackQueryHandler(learn_sexual_grooming, pattern="^learn_sexual_grooming$"))
+    application.add_handler(CallbackQueryHandler(learn_sexual_grooming2, pattern="^learn_sexual_grooming2$"))
+    application.add_handler(CallbackQueryHandler(learn_sexual_grooming3, pattern="^learn_sexual_grooming3$"))
+    application.add_handler(CallbackQueryHandler(learn_consent, pattern="^learn_consent$"))
+    application.add_handler(CallbackQueryHandler(learn_consent2, pattern="^learn_consent2$"))
+    application.add_handler(CallbackQueryHandler(learn_consent3, pattern="^learn_consent3$"))
+    application.add_handler(CallbackQueryHandler(learn_consent4, pattern="^learn_consent4$"))
+    application.add_handler(CallbackQueryHandler(learn_victim_blaming, pattern="^learn_victim_blaming$"))
+    application.add_handler(CallbackQueryHandler(learn_victim_blaming2, pattern="^learn_victim_blaming2$"))
+    application.add_handler(CallbackQueryHandler(learn_rape_myths, pattern="^learn_rape_myths$"))
+
     # Story submission handler
     application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_story))
     
